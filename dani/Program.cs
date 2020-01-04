eb1bb1ef51cc645e3aba1cfa0655cbe1240098d5
Daniel Sauer <dansau@microsoft.com>
2020-01-04 17:47:03 -0300

    fvfdvfvd

---
+++ dani/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dani
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
